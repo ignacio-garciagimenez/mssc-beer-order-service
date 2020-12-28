f13ccfac00c37b8b8b86cdee8950f88dc2c4fd57
Ignacio Garcia Gimenez <ignaciogarciagimenez@gmail.com>
2020-12-28 19:45:58 -0300

    Hotifx, changing customercontroller base path and adding service annotation to CustomerServiceImpl

--- src/main/java/guru/sfg/beer/order/service/services/CustomerServiceImpl.java
+++ src/main/java/guru/sfg/beer/order/service/services/CustomerServiceImpl.java
@@ -8,10 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Service
 public class CustomerServiceImpl implements  CustomerService {
 
     private final CustomerRepository customerRepository;
